f4adc88ed9139c273441bee0bbbca640b9207e3f
Philip Bove <phil@bove.online>
2020-11-30 18:55:22 -0500

    added some basic elasticsearch stuff

---
+++ boundless/__init__.py
@@ -0,0 +1,6 @@
+from flask import Flask
+from flask_restx import Api
+
+app = Flask(__name__)
+api = Api(app)
+
---
+++ tests/integration/test_elasticsearch.py
@@ -0,0 +1,31 @@
+from boundless.databases import ElasticsearchConnector
+from elasticsearch7 import Elasticsearch
+import atexit
+import os
+
+def remove_all_indices():
+	es = Elasticsearch(nodes=['localhost'], use_ssl=False)
+	[es.indices.delete(x) for x in es.indices.get('*').keys()]
+
+if not os.getenv('CLEANUP'):
+	atexit.register(remove_all_indices)
+
+def test_get_active_index_creation():
+	elastic = ElasticsearchConnector(nodes=['localhost'], use_ssl=False)
+	elastic.create_index('wiretap')
+	elastic.create_index('wiretap')
+	elastic.create_index('wiretap')
+	assert elastic.get_active_index('wiretap') == 'wiretap_3'
+
+def test_index_rotation():
+	es = Elasticsearch(['localhost'])
+	elastic = ElasticsearchConnector(nodes=['localhost'], use_ssl=False)
+	elastic.create_index('test_rotate')
+	elastic.rotate_index('test_rotate')
+	assert elastic.get_active_index('test_rotate') == 'test_rotate_2'
+	assert es.indices.get('test_rotate_1')['test_rotate_1']['settings']['index']['blocks']['write']
+
+def test_message_insert():
+	elastic = ElasticsearchConnector(nodes=['localhost'], use_ssl=False)
+	index = elastic.get_active_index('wiretap')
+
